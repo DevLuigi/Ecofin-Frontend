fd1dc2ac3d6d9da1df4c8929025a6a71fe35ccf5
André Santos da Silva <andreew.0003@gmail.com>
2024-11-27 20:18:51 -0300

    fix: Merge conflict issue-14

--- src/pages/dashboard-category/index.js
+++ src/pages/dashboard-category/index.js
@@ -1,4 +1,5 @@
-import Conteudo from "../../components/conteudo-principal"
+import Button from "../../components/button";
+import Conteudo from "../../components/conteudo-principal";
 import HeaderDashboard from "../../components/header-dashboard";
 
 import { Container } from "./styled";
@@ -9,7 +10,53 @@ export default function Categorias() {
             <HeaderDashboard />
 
             <Conteudo myTitle={"Categorias"}>
+                <div className="createCategoryButton">
+                    <Button
+                        myMargin={"0em"}
+                        myHeight={6}
+                        myWidth={10}
+                        myBackgroundColor={"#A8D8B5"}
+                        myColor={"#000000"}
+                        myMethod={() => alert("Criar nova categoria")}
+                        myImagePath={"/assets/images/mais.png"}
+                        > 
+                        Criar categoria 
+                    </Button>
+                </div>
                 
+                <section className="buttons">                    
+                    <div>
+                        <Button
+                            myMargin={"0em"}
+                            myHeight={6}
+                            myWidth={9}
+                            isTransparent={true}
+                            myColor={"#707070"}
+                            myMethod={() => alert("Despesas")}
+                        > 
+                            Despesas 
+                        </Button>
+                        
+                    </div>
+
+                    <div>
+                        <Button
+                            myMargin={"0em"}
+                            myHeight={6}
+                            myWidth={9}
+                            isTransparent={true}
+                            myColor={"#707070"}
+                            myMethod={() => alert("Receitas")}
+                        > 
+                            Receitas 
+                        </Button>
+                    </div>                    
+                </section>
+
+                <section className="categoryList">
+                
+                </section>
+
             </Conteudo>
         </Container>
     )
